cea8bd4f19b11296f61bab05ed25a10df60132db
Sarah Adams <sadams.codes@gmail.com>
2014-02-27 15:35:53 -0800

    remove manual env set

--- cmd/goose/main.go
+++ cmd/goose/main.go
@@ -15,9 +15,6 @@ var flagEnv = flag.String("env", os.Getenv("GO_ENV"), "which DB environment to u
 
 // helper to create a DBConf from the given flags
 func dbConfFromFlags() (dbconf *goose.DBConf, err error) {
-	envStr := os.Getenv("GO_ENV")
-	flagEnv = &envStr
-
 	return goose.NewDBConf(*flagPath, *flagEnv)
 }
 
